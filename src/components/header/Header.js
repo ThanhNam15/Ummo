a29a4a3a1d1a2b9c35a0ffbf56b26dfffde685f0
NamNguyen <ntnam150820@gmail.com>
2024-03-15 20:29:48 +0700

    header

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -38,21 +38,33 @@ export default function Header() {
     <div className="header fixed-top">
     <Container>
       <Navbar expand="md">
-        <NavbarBrand to="/"><img src={logo} alt="logo" width="100px"/></NavbarBrand>
+        <NavbarBrand>
+          <Link to='/'>
+            <img src={logo} alt="logo" width="100px"/>
+          </Link>
+        </NavbarBrand>
         <NavbarToggler className="header_toggle" onClick={toggle}><AiOutlineMenu color="var(--black-color)" /></NavbarToggler>
         <Collapse isOpen={isOpen} navbar>
           <Nav className="mx-auto" navbar>
             <NavItem className="mx-3">
-              <NavLink className="header_text" style={{fontWeight: "600"}} to='/'>HOME</NavLink>
+              <Link to='/' style={{textDecoration: "none"}}>
+                <NavLink className="header_text" style={{fontWeight: "600"}}>HOME</NavLink>
+              </Link>
             </NavItem>
             <NavItem className="mx-3">
-              <NavLink className="header_text" style={{fontWeight: "600"}} to='/shop'>SHOP</NavLink>
+              <Link to='/shop' style={{textDecoration: "none"}}>
+                <NavLink className="header_text" style={{fontWeight: "600"}} to='/shop'>SHOP</NavLink>
+              </Link>
             </NavItem>
             <NavItem className="mx-3">
-              <NavLink className="header_text" style={{fontWeight: "600"}} to='/contact'>CONTACT</NavLink>
+              <Link to='/contact' style={{textDecoration: "none"}}>
+                <NavLink className="header_text" style={{fontWeight: "600"}} to='/contact'>CONTACT</NavLink>
+              </Link>
             </NavItem>
             <NavItem className="mx-3">
-              <NavLink className="header_text" style={{fontWeight: "600"}} to='/blog/'>BLOG</NavLink>
+              <Link to='/blog' style={{textDecoration: "none"}}>
+                <NavLink className="header_text" style={{fontWeight: "600"}} to='/blog/'>BLOG</NavLink>
+              </Link>
             </NavItem>
           </Nav>
           <Nav>
